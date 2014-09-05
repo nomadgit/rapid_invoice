93021f0026e34fd61326e44664b34072f003cd9b
Mark Bubien <markbubien@gmail.com>
2014-09-05 13:06:09 +0100

    Rspec tests for client validation

---
+++ spec/factories/clients.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+	factory :client do
+		name "Johnny's Autos"
+		address "1 New Road, London, SW6 1AB"
+		email "info@johnnysautos.com"
+	end
+end
---
+++ spec/models/client_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Client do
+  it "is invalid without a name" do
+  	expect(build(:client, name: nil)).to have(1).errors_on(:name)
+  end
+  it "is invalid without an address" do
+  	expect(build(:client, address: nil)).to have(1).errors_on(:address)
+  end
+  it "is invalid without an email" do
+  	expect(build(:client, email: nil)).to have(2).errors_on(:email)
+  end
+  it "is invalid if the email is in an incorrect format" do
+  	expect(build(:client, email: 'xxx')).to have(1).errors_on(:email)
+  	expect(build(:client, email: 'xxx@yyy')).to have(1).errors_on(:email)
+  	expect(build(:client, email: 'xxx.com')).to have(1).errors_on(:email)
+  	expect(build(:client, email: 'xx%x@yyy.com')).to have(1).errors_on(:email)
+  	expect(build(:client, email: 'xxx@yyy.zz-zz.com')).to have(1).errors_on(:email)
+  end
+  it "is valid with a name, an address and an email in the correct format" do
+  	expect(build(:client)).to be_valid 
+  end
+  
+end
