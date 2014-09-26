6c50221d718f794343499a8348fe88c5b32511e0
Mark Bubien <markbubien@gmail.com>
2014-09-26 15:49:57 +0100

    Ensure initial redirects in Client controller work

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,38 +6,32 @@ class ClientsController < ApplicationController
   end
 
   def show
-    @client = Client.find params[:id]
   end
 
   def new
     @client = Client.new
   end
 
-  # GET /clients/1/edit
   def edit
   end
 
-  # POST /clients
   def create
     @client = Client.new(client_params)
-
     if @client.save
-      redirect_to @client, notice: 'Client was successfully created.'
+      redirect_to clients_url, notice: 'Client was successfully created.'
     else
       render :new
     end
   end
 
-  # PATCH/PUT /clients/1
   def update
     if @client.update(client_params)
-      redirect_to @client, notice: 'Client was successfully updated.'
+      redirect_to clients_url, notice: 'Client was successfully updated.'
     else
       render :edit
     end
   end
 
-  # DELETE /clients/1
   def destroy
     @client.destroy
     redirect_to clients_url, notice: 'Client was successfully destroyed.'
